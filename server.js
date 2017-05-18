197c7831e8bede96126346f4b49fbabe43cc1134
Guilherme Goncalves <guilhermemordente@gmail.com>
2017-05-17 23:17:24 -0700

    adding dummy mongo db persistance

--- server.js
+++ server.js
@@ -2,52 +2,78 @@ import fs from 'fs'
 import restify from 'restify'
 import R from 'ramda'
 import socketIo from 'socket.io'
+import mongo from 'mongodb'
 
-const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || `127.0.0.1`
+const serverPort = 8080
+const serverIpAddress = `127.0.0.1`
 
 const dbName = `bc`
-const mongoHost = `mongodb://127.0.0.1:27017/` || process.env.OPENSHIFT_MONGODB_DB_URL
+const mongoHost = `mongodb://127.0.0.1:27017/`
 const mongoUrl = mongoHost + dbName
-console.log(`mongo url: ` + mongoUrl)
 
 const app = restify.createServer()
 const io = socketIo(app.server)
  
-app.get(`/`, function( req, res ){
-  fs.readFile(`index.html`,function (err, data){
-    res.writeHead(200, {
-      [`Content-Type`]: `text/html`,
-      [`Content-Length`]: data.length
+mongo.connect(mongoUrl, (err, db) => {
+  if (err) {
+    console.log(`err`, err)
+    return
+  }
+  console.log(`Succesfully connected to the database`)
+  app.get(`/`, ( req, res ) => {
+    fs.readFile(`index.html`, (err, data) => {
+      res.writeHead(200, {
+        [`Content-Type`]: `text/html`,
+        [`Content-Length`]: data.length
+      })
+      res.write(data)
+      res.end()
+    })}
+  )
+  app.get(/\/public\/?.*/, restify.serveStatic({ directory: __dirname }))
+
+  const MESSAGE = `message`
+  const CONNECT = `connection`
+  const DISCONNECT = `disconnect`
+
+  const disconnectHandler = R.curry((socket, event) => {
+    console.log(`[${event}] user ${socket.id} disconnected`)
+  })
+
+  const connectionHandler = (socket) => {
+    console.log(`User ${socket.id} connected`)
+
+    const messages = db.collection(`messages`)
+    messages.find({}).toArray((err, result) => {
+      if (err) {
+        console.log(`err`, err)
+        return
+      }
+      
+      result.map(({name, msg}) => {
+        io.emit(MESSAGE, `[${name}]: ${msg}`)
+      })
     })
-    res.write(data)
-    res.end()
-  })}
-)
- 
-app.get(/\/public\/?.*/, restify.serveStatic({ directory: __dirname }))
 
-const MESSAGE = `message`
-const CONNECT = `connection`
-const DISCONNECT = `disconnect`
+    socket.on(MESSAGE, messageHandler(socket))
+    socket.on(DISCONNECT, disconnectHandler(socket))
+  }
 
-const disconnectHandler = R.curry((socket, event) => {
-  console.log(`[${event}] user ${socket.id} disconnected`)
+  const messageHandler = R.curry((socket, msg, name) => {
+    console.log(`[${name}]: `, msg)
+    const messages = db.collection(`messages`)
+    messages.insert({name, msg}, (err, result) => {
+      if (err) {
+        console.log(`err`, err)
+        return
+      }
+      console.log(`Inserted sucessfully into the database`, result.ops)
+    })
+    io.emit(MESSAGE, `[${name}]: ${msg}`)
+  })
+
+  io.on(CONNECT, connectionHandler)
+
+  app.listen( serverPort, serverIpAddress )
+  console.log( `Server is running on 8080` )
 })
-
-const connectionHandler = (socket) => {
-  console.log(`User ${socket.id} connected`)
-
-  socket.on(MESSAGE, messageHandler(socket))
-  socket.on(DISCONNECT, disconnectHandler(socket))
-}
-
-const messageHandler = R.curry((socket, msg, name) => {
-  console.log(`[${name}]: `, msg)
-  io.emit(MESSAGE, `[${name}]: ${msg}`)
-})
-
-io.on(CONNECT, connectionHandler)
-
-app.listen( serverPort, serverIpAddress )
-console.log( `Server is running on 8080` )
