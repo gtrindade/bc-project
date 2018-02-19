f5e24db5b70a89196fd8895928eac195a03b5585
Guilherme Goncalves <ggoncalves@fanatics.com>
2018-02-18 20:34:32 -0800

    parametrizing the ping chat

--- src/be/services/multi-kiew/multi-kiew.js
+++ src/be/services/multi-kiew/multi-kiew.js
@@ -1,17 +1,21 @@
 import request from 'request'
 
-const {TELEGRAM_BOT_TOKEN} = process.env
+const {
+  TELEGRAM_BOT_TOKEN,
+  PING_CHAT,
+} = process.env
 
 const domain = `https://api.telegram.org`
 const bot = `/bot${TELEGRAM_BOT_TOKEN}`
-const pingChat = -1001320690244
+// const pingBC = -1001320690244
+// const bcNew = -1001313196804
 const sendMessagePath = `/sendMessage`
 const getMessageQuery = (chatId, message) => `?chat_id=${chatId}&text=${message}`
 
 const messageEndpoint = (id, msg) => `${domain}${bot}${sendMessagePath}${getMessageQuery(id, msg)}`
 
 const sendMessage = (msg) => new Promise((resolve, reject) => {
-  return request.post(messageEndpoint(pingChat, msg), {}, (error, response) => {
+  return request.post(messageEndpoint(PING_CHAT, msg), {}, (error, response) => {
     if (error) { 
       reject(error)
     }
